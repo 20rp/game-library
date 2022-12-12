17c66c0016f83f0cc45c5012c1cd3ca0d6e464f6
Arlo Wotherspoon <awotherspoon@eit.ac.nz>
2022-12-12 14:47:06 +1300

    Added nodejs gitignore

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-const port = 3000;
+const port = 8000;
 
 app.get('/', function (req, res) {
     res.send("Hello world");
 });
 
 app.get('/insert', function (req, res) {
-    res.send("Hello inserter");
+    res.send("Hello world!");
 })
 
 app.listen(port);
